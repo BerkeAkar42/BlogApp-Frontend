09425c67839ef8f66d4f2544060f7e9b978050cd
Berke Akar <berkeakar4234@gmail.com>
2026-04-25 17:21:05 +0300

    feat: Footer component has been added

--- src/pages/components/footer/Footer.tsx
+++ src/pages/components/footer/Footer.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import styles from '../footer/footer.module.css'
+// {`${styles.}`}
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={`mt-5`}>
+      <div className={`container`}>
+        <div className={`row ${styles["footer-row"]}`}>
+          <div className={`${styles.col} col col-1 col-sm-2 col-md-3`}></div>
+          <div className={`${styles.col} col`}>İçeriklerinizi Paylaşabileceğiniz Ücretsiz Bir Servis <br/> BlogApp</div>
+          <div className={`${styles.col} col col-1 col-sm-2 col-md-3`}></div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
