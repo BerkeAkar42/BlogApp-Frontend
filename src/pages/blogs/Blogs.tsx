2d0e33cc6b0980c1cc0a1f543311a146f2728e37
Berke Akar <berkeakar4234@gmail.com>
2026-04-26 16:47:51 +0300

    feat: connect blogs API and display blogs on UI

--- src/pages/blogs/Blogs.tsx
+++ src/pages/blogs/Blogs.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from '../blogs/blogs.module.css'
+import BlogList from '../../components/blogList/BlogList'
+// {``}
+// {`${}`}
 
 function Blogs() {
   return (
-    <div>Blogs</div>
+    <div className={`container mt-3`}>
+      <div className={`row ${styles["blogs-row-1"]}`}>
+        <div className={`col-12 ${styles["blogs-title"]}`}><h2>Güncel Bloglar</h2></div>
+      </div>
+      <div className={`row ${styles["blogs-row-2"]}`}>
+        <div className={`col`}>
+          <BlogList />
+        </div>
+      </div>
+    </div>
   )
 }
 
